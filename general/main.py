c83be015d32081cee1a89c767de2576016bbcada
LightAcronym <orgewe@gmail.com>
2023-12-24 16:59:39 +0200

    Add main.py, utilities.py, config.json

--- general/main.py
+++ general/main.py
@@ -5,32 +5,38 @@ from general.database.connect_insert_db import insert_data
 from general.database.create_db import create_tables
 from general.preprocess_data_for_enrichment import preprocess_yellow_pages_an_yelp
 from general.web_scraper import scrape
-from general.utilities import load_constants, load_db_config
+from general.utilities import load_constants, load_db_config, load_db_config, parse, flags
 
 
 def main():
-    scraped_dict = scrape(load_constants().get('BASE_URL'))
+    flag_show, flag_excel, flag_insert_data_base = parse()
+    flag_scrape = flags(flag_show, flag_excel, flag_insert_data_base)
+    # next line for debugging, can be deleted at the end or for pycharm use:
+    # flag_show, flag_excel, flag_insert_data_base, flag_scrape = True, True, True, True
+    if flag_scrape:
+        scraped_dict = scrape(load_constants().get('BASE_URL'))
+        if flag_show:
+            print_the_scraping_data(scraped_dict)
+        if flag_excel:
+            scraped_yellow_page_data_to_csv(scraped_dict, load_constants().get('YELLOW_PAGES_CSV_FILE'))
+        if flag_insert_data_base:
+            # API_KEY kept in SECRET,
+            # so only_matched_yelp.csv and raw yelp files were uploaded to GitHub
+            secret_api = False  # "Don't change"
+            if secret_api:
+                get_enrichment_data_to_csv(load_constants().get('YELLOW_PAGES_CSV_FILE'),
+                                           load_constants().get('ENDPOINT'),
+                                           load_constants().get('HEADERS'))
 
-    print_the_scraping_data(scraped_dict)
-    scraped_yellow_page_data_to_csv(scraped_dict, load_constants().get('YELLOW_PAGES_CSV_FILE'))
+            create_db.build_database(load_db_config().get("database"), load_db_config().get("host"),
+                                     load_db_config().get("user"), load_db_config().get("password"),
+                                     create_tables())
 
-    # API_KEY kept in SECRET,
-    # so only_matched_yelp.csv and raw yelp files were uploaded to GitHub
-    secret_api = False  # Don't change
-    if secret_api:
-        get_enrichment_data_to_csv(load_constants().get('YELLOW_PAGES_CSV_FILE'),
-                                   load_constants().get('ENDPOINT'),
-                                   load_constants().get('HEADERS'))
+            preprocess_yellow_pages_an_yelp(load_constants().get('YELP_RAW_CSV_FILE'),
+                                            load_constants().get('YELLOW_PAGES_CSV_FILE'))
 
-    create_db.build_database(load_db_config().get("database"), load_db_config().get("host"),
-                             load_db_config().get("user"), load_db_config().get("password"),
-                             create_tables())
-
-    preprocess_yellow_pages_an_yelp(load_constants().get('YELP_RAW_CSV_FILE'),
-                                    load_constants().get('YELLOW_PAGES_CSV_FILE'))
-
-    print("+++ Inserting the scraped data into the database +++")
-    insert_data(scraped_dict, load_db_config())
+            print("+++ Inserting the scraped data into the database +++")
+            insert_data(scraped_dict, load_db_config())
 
 
 if __name__ == "__main__":
--- general/utilities.py
+++ general/utilities.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+import argparse
 from general.logger import logger
 
 
@@ -36,3 +36,27 @@ def load_db_config():
         return config_data.get('DB_CONFIG', {})
     else:
         return {}
+
+
+def parse():
+    parser = argparse.ArgumentParser(prog='yellow_pages_web_scraper', description="yellow pages web scraper",
+                                     epilog='bon appetit')
+    parser.add_argument('-sh', '--show-everything', action='store_true',
+                        help='optional, show the relevant data')
+    parser.add_argument('-csv', '--export-csv', action='store_true',
+                        help='optional, import the data to csv file')
+    parser.add_argument('-idb', '--insert-data-base', action='store_true',
+                        help='optional, insert the data to mysql')
+    args = parser.parse_args()
+    print("nothing will be shown, try adding -sh to see on terminal") if not args.show_everything else ""
+    print("nothing will be exported, try adding -csv to export to csv file") if not args.export_csv else ""
+    print("nothing will be insert, try adding -idb to export to MySQL") if not args.insert_data_base else ""
+    return args.show_everything, args.export_csv, args.insert_data_base
+
+
+def flags(flag_show, flag_excel, flag_insert_data_base):
+    flag_scrape = False
+    list_flags = [flag_show, flag_excel, flag_insert_data_base]
+    if any(list_flags):
+        flag_scrape = True
+    return flag_scrape
